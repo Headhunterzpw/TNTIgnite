90bc5a2f5aafa6569e76f10b56f803428021a9c6
AgentTroll <woodyc40@gmail.com>
2013-08-05 15:33:48 -0700

    Update TNTIgnite.java

--- src/com/gmail/woodyc40/tntignite/TNTIgnite.java
+++ src/com/gmail/woodyc40/tntignite/TNTIgnite.java
@@ -1,3 +1,9 @@
-public class TNTIgnite extends JavaPlugin{
-
-}
+package com.gmail.woodyc40.tntignite;
+import org.bukkit.Material;
+import org.bukkit.entity.TNTPrimed;
+import org.bukkit.event.*;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+public class TNTIgnite extends JavaPlugin{@Override
+  public void onEnable(){getServer().getPluginManager().registerEvents(new Listener(){@EventHandler
+			public void onPlace(BlockPlaceEvent e){if(e.getBlock().getType().equals(Material.TNT)){if(e.getPlayer().hasPermission("tnt.ignite")){e.setCancelled(true);e.getBlock().getWorld().spawn(e.getBlock().getLocation().add(0.5, 0.25, 0.5), TNTPrimed.class);}else{return;}}}}, this);}}
